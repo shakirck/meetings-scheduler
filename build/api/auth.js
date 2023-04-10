ae6e2377e8736855f91a18e2ac1613aef5577f4d
Muhammed Shakir C K <shakirck333@gmail.com>
2023-04-10 19:04:50 +0530

    build changes

--- build/api/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../app"));
-const auth_1 = __importDefault(require("../routes/auth/auth"));
-app_1.default.use('/auth', auth_1.default);
